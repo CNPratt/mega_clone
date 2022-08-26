c0cb75551eb6384b696a4140a481d7dec1d4df6e
Christopher Pratt <76122198+CNPratt@users.noreply.github.com>
2022-08-25 21:39:22 -0400

    more updates and adjustments

--- Assets/Scripts/PM2.cs
+++ Assets/Scripts/PM2.cs
@@ -11,9 +11,10 @@ public class PM2 : MonoBehaviour
     public GameObject bullet;
 
     private bool facingRight = true;
-    private bool isGrounded = false;
+    public bool isGrounded = false;
     public bool isShooting = false;
     public bool isSliding = false;
+    public bool canSlide = true;
     public bool jump = false;
     public bool fireButton = false;
 
@@ -22,8 +23,6 @@ public class PM2 : MonoBehaviour
 
     int slideMask;
 
-    public Vector3 groundRayOffset = new Vector3(0, 0.01f, 0);
-    public Vector3 slideRayOffset = new Vector3(0, 0, 0);
     public Vector3 playerVelocity = new Vector2(0, 0);
     public Vector3 vec = new Vector3(0, 0, 0);
 
@@ -31,9 +30,14 @@ public class PM2 : MonoBehaviour
     public Vector2 jumpInitialVec = new Vector3(0, .5f, 0);
     public Vector2 jumpVec = new Vector2(0, 250);
 
-    RaycastHit2D groundHit;
-    RaycastHit2D rightHit;
-    RaycastHit2D leftHit;
+    List<RaycastHit2D> lateralHits;
+    List<RaycastHit2D> groundHits;
+    RaycastHit2D rightTopHit;
+    RaycastHit2D rightMiddleHit;
+    RaycastHit2D rightBottomHit;
+    RaycastHit2D leftTopHit;
+    RaycastHit2D leftMiddleHit;
+    RaycastHit2D leftBottomHit;
 
     void SetAnimParams()
     {
@@ -58,20 +62,34 @@ public class PM2 : MonoBehaviour
         bool shouldFlipSprite = false;
         float thisMovement = 1f;
         float shotpointOffset = -.6f;
-        RaycastHit2D wallslideHit = rightHit;
+        RaycastHit2D lateralTopHit = rightTopHit;
+        RaycastHit2D lateralMidHit = rightMiddleHit;
+        RaycastHit2D lateralBottomHit = rightBottomHit;
         Vector2 wallslideJumpVec = new Vector2(-4, 6);
         Vector3 jumpInitVec = new Vector3(-.1f, .1f, 0);
 
+        if (rightBottomHit || rightMiddleHit || rightTopHit)
+        {
+            thisMovement = 0f;
+        }
+
         if (!isRight)
         {
-            // Is holding directional right input down
+            // Is holding directional left input down
 
             isFacingRight = false;
             shouldFlipSprite = true;
             thisMovement = -1f;
             shotpointOffset = .6f;
-            wallslideHit = leftHit;
+            lateralTopHit = leftTopHit;
+            lateralMidHit = leftMiddleHit;
+            lateralBottomHit = leftBottomHit;
             wallslideJumpVec = new Vector2(4, 6);
+
+            if (leftBottomHit || leftMiddleHit || leftTopHit)
+            {
+                thisMovement = 0f;
+            }
         }
 
         movement = thisMovement;
@@ -87,9 +105,13 @@ public class PM2 : MonoBehaviour
             shotPoint.transform.position = new Vector3(rb.position.x + -shotpointOffset, rb.position.y, 0);
         }
 
-        if (wallslideHit.collider == true && !isGrounded)
+        if (lateralMidHit.collider == true && !isGrounded)
         {
-            isSliding = true;
+            //if (canSlide)
+            //{
+                isSliding = true;
+            //}
+
             slideJumpVec = wallslideJumpVec;
             jumpInitialVec = jumpInitVec;
         }
@@ -117,22 +139,52 @@ public class PM2 : MonoBehaviour
         }
     }
 
-    private RaycastHit2D GroundedCast()
-    {
-        Debug.DrawRay(transform.position - (Vector3.up / 2) - groundRayOffset, -Vector2.up / 100);
-        return Physics2D.Raycast(transform.position - (Vector3.up / 2), -Vector2.up, .01f, slideMask);
-    }
-
-    private List<RaycastHit2D> WallslideCasts()
+    private List<RaycastHit2D> GroundedCasts()
     {
         List<RaycastHit2D> hits = new List<RaycastHit2D>();
-        RaycastHit2D rightMid = Physics2D.Raycast(transform.position - (Vector3.left / 4), -Vector2.left, .01f, slideMask);
-        Debug.DrawRay(transform.position - (Vector3.left / 4) + slideRayOffset, -Vector2.left / 100);
-        RaycastHit2D leftMid = leftHit = Physics2D.Raycast(transform.position + (Vector3.left / 4), Vector2.left, .01f, slideMask);
-        Debug.DrawRay(transform.position + (Vector3.left / 4) - slideRayOffset, Vector2.left / 100);
 
+        Debug.DrawRay(transform.position + new Vector3(-.24f, -.5f, 0), -Vector2.up / 100);
+        RaycastHit2D bottomLeft = Physics2D.Raycast(transform.position + new Vector3(-.24f, -.5f, 0), -Vector2.up, .01f, slideMask);
+        hits.Add(bottomLeft);
+
+        Debug.DrawRay(transform.position + new Vector3(0, -.5f, 0), -Vector2.up / 100);
+        RaycastHit2D bottomMid = Physics2D.Raycast(transform.position + new Vector3(0, -.5f, 0), -Vector2.up, .01f, slideMask);
+        hits.Add(bottomMid);
+
+        Debug.DrawRay(transform.position + new Vector3(.24f, -.5f, 0), -Vector2.up / 100);
+        RaycastHit2D bottomRight = Physics2D.Raycast(transform.position + new Vector3(.24f, -.5f, 0), -Vector2.up, .01f, slideMask);
+        hits.Add(bottomRight);
+
+        return hits;
+    }
+
+    private List<RaycastHit2D> LateralCasts()
+    {
+        List<RaycastHit2D> hits = new List<RaycastHit2D>();
+
+        RaycastHit2D rightTop = Physics2D.Raycast(transform.position + new Vector3(.25f, .5f, 0), -Vector2.left, .01f, slideMask);
+        Debug.DrawRay(transform.position + new Vector3(.25f, .5f, 0), new Vector2(.01f, 0));
+        hits.Add(rightTop);
+
+        RaycastHit2D rightMid = Physics2D.Raycast(transform.position + new Vector3(.25f, 0, 0), -Vector2.left, .01f, slideMask);
+        Debug.DrawRay(transform.position + new Vector3(.25f, 0, 0), new Vector2(.01f, 0));
         hits.Add(rightMid);
+
+        RaycastHit2D rightBottom = Physics2D.Raycast(transform.position + new Vector3(.25f, -.5f, 0), -Vector2.left, .01f, slideMask);
+        Debug.DrawRay(transform.position + new Vector3(.25f, -.5f, 0), new Vector2(.01f, 0));
+        hits.Add(rightBottom);
+
+        RaycastHit2D leftTop = Physics2D.Raycast(transform.position + new Vector3(-.25f, .5f, 0), Vector2.left, .01f, slideMask);
+        Debug.DrawRay(transform.position + new Vector3(-.25f, .5f, 0), new Vector2(-.01f, 0));
+        hits.Add(leftTop);
+
+        RaycastHit2D leftMid = Physics2D.Raycast(transform.position + new Vector3(-.25f, 0, 0), Vector2.left, .01f, slideMask);
+        Debug.DrawRay(transform.position + new Vector3(-.25f, 0, 0), new Vector2(-.01f, 0));
         hits.Add(leftMid);
+
+        RaycastHit2D leftBottom = Physics2D.Raycast(transform.position + new Vector3(-.25f, -.5f, 0), Vector2.left, .01f, slideMask);
+        Debug.DrawRay(transform.position + new Vector3(-.25f, -.5f, 0), new Vector2(-.01f, 0));
+        hits.Add(leftBottom);
 
         return hits;
     }
@@ -146,11 +198,19 @@ public class PM2 : MonoBehaviour
         }
     }
 
+    private IEnumerator CanSlideToTrue()
+    {
+        yield return new WaitForSeconds(.1f);
+        if (!canSlide)
+        {
+            canSlide = true;
+        }
+    }
+
     private IEnumerator WallJumpDeflector()
     {
         yield return new WaitForSeconds(.3f);
         rb.AddForce(new Vector2(-slideJumpVec.x, 2), ForceMode2D.Impulse);
-        Debug.Log("ran");
     }
 
     private IEnumerator JumpRoutine()
@@ -165,6 +225,27 @@ public class PM2 : MonoBehaviour
             float proportionCompleted = timer / jumpTime;
             Vector2 thisFrameJumpVector = Vector2.Lerp(Vector2.up, Vector2.zero, proportionCompleted);
             rb.AddForce(thisFrameJumpVector * Time.deltaTime * 50, ForceMode2D.Impulse);
+            //transform.Translate(thisFrameJumpVector/10);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        // jumping = false;
+    }
+
+    private IEnumerator Walljump()
+    {
+        rb.velocity = Vector2.zero;
+        Vector2 jumpVector = rb.position;
+        float jumpTime = .2f;
+        float timer = 0;
+
+        while (timer < jumpTime)
+        {
+            float proportionCompleted = timer / jumpTime;
+            Vector2 thisFrameJumpVector = Vector2.Lerp(Vector2.up, slideJumpVec, proportionCompleted);
+            //rb.AddForce(thisFrameJumpVector * Time.deltaTime * 50, ForceMode2D.Impulse);
+            transform.Translate(thisFrameJumpVector / 50);
             timer += Time.deltaTime;
             yield return null;
         }
@@ -195,21 +276,36 @@ public class PM2 : MonoBehaviour
         SetAnimParams();
 
         // Grounding raycast
-        groundHit = GroundedCast();
+        groundHits = GroundedCasts();
 
         // Sliding raycasts
-        List<RaycastHit2D> wallslideHits = WallslideCasts();
-        rightHit = wallslideHits[0];
+        lateralHits = LateralCasts();
+        rightMiddleHit = lateralHits[1];
 
-        leftHit = wallslideHits[1];
+        leftMiddleHit = lateralHits[4];
 
-        if (groundHit.collider == true)
+        //Debug.Log(groundHits[1]);
+
+        //if (groundHits[1].collider == true)
+        //{
+        //    isGrounded = true;
+        //}
+        //else
+        //{
+        //    isGrounded = false;
+        //}
+
+        foreach (RaycastHit2D hit in groundHits)
         {
-            isGrounded = true;
-        }
-        else
-        {
-            isGrounded = false;
+            if (hit.collider == true)
+            {
+                isGrounded = true;
+                break;
+            }
+            else
+            {
+                isGrounded = false;
+            }
         }
 
         if (isSliding)
@@ -243,6 +339,9 @@ public class PM2 : MonoBehaviour
 
         if (jump && isGrounded)
         {
+            //canSlide = false;
+            //StopCoroutine("CanSlideToTrue");
+            //StartCoroutine("CanSlideToTrue");
             StartCoroutine("JumpRoutine");
             // rb.AddForce(jumpVec);
         }
@@ -250,6 +349,8 @@ public class PM2 : MonoBehaviour
         if (jump && isSliding)
         {
             rb.AddForce(slideJumpVec, ForceMode2D.Impulse);
+            //StopCoroutine("Walljump");
+            //StartCoroutine("Walljump");
             // StopCoroutine("WallJumpDeflector");
             // StartCoroutine("WallJumpDeflector");
         }
@@ -263,15 +364,16 @@ public class PM2 : MonoBehaviour
                 speedFactor = 2;
             }
             transform.Translate(new Vector3(movement * speedFactor, 0, 0) * Time.deltaTime);
+
         }
 
-        if (movement == 0)
-        {
-            rb.velocity = new Vector2(0, rb.velocity.y);
-        }
+        //if (movement == 0)
+        //{
+        //    rb.velocity = new Vector2(0, rb.velocity.y);
+        //}
     }
 
-    void FixedUpdated()
+    void FixedUpdate()
     {
 
     }
